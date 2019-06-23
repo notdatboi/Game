d9cebe6e1003aeea705a5a56d82734e65a325a31
notdatboi <noddatboi@gmail.com>
2019-06-23 10:46:43 +0300

    Added file, which includes every Spark  header

---
+++ include/Spark.hpp
@@ -0,0 +1,12 @@
+#ifndef SPARK_SPARK_HPP
+#define SPARK_SPARK_HPP
+#include"DescriptorPool.hpp"
+#include"Buffer.hpp"
+#include"Image.hpp"
+#include"ImageView.hpp"
+#include"Pipeline.hpp"
+#include"RenderPass.hpp"
+#include"ShaderSet.hpp"
+#include"Subpass.hpp"
+#include"Swapchain.hpp"
+#endif
