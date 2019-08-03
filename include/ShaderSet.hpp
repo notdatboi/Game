2b6630bd906bec664ad200f0f50d78c88a4e651b
notdatboi <noddatboi@gmail.com>
2019-08-03 21:12:48 +0300

    Adapted ShaderSet class to Shader class

--- include/ShaderSet.hpp
+++ include/ShaderSet.hpp
@@ -6,31 +6,33 @@
 #include<string>
 #include<fstream>
 #include"System.hpp"
+#include<Shader.hpp>
 
 namespace spk
 {
-    struct ShaderInfo
-    {
-        vk::ShaderStageFlagBits type;
-        std::string filename;
-    };
+    //struct ShaderInfo
+    //{
+    //    vk::ShaderStageFlagBits type;
+    //    std::string filename;
+    //};
 
     class ShaderSet
     {
     public:
         ShaderSet();
-        ShaderSet(const ShaderSet& set);
-        ShaderSet(const std::vector<ShaderInfo>& shaders);
-        void create(const std::vector<ShaderInfo>& shaders);
-        ShaderSet& operator=(const ShaderSet& set);
+        //ShaderSet(const ShaderSet& set);
+        ShaderSet(const std::vector<std::string>& shaderFilenames/*const std::vector<ShaderInfo>& shaders*/);
+        void create(const std::vector<std::string>& shaderFilenames/*const std::vector<ShaderInfo>& shaders*/);
+        //ShaderSet& operator=(const ShaderSet& set);
         const std::vector<vk::PipelineShaderStageCreateInfo>& getShaderStages() const;
         const uint32_t getIdentifier() const;
         ~ShaderSet();
     private:
-        std::vector<ShaderInfo> infos;
-        std::vector<char> getCode(const std::string& filename) const;
+        //std::vector<ShaderInfo> infos;
+        //std::vector<char> getCode(const std::string& filename) const;
         std::vector<vk::PipelineShaderStageCreateInfo> shaderStages;
-        std::vector<std::pair<vk::ShaderModule, vk::ShaderStageFlagBits> > shaderModules;
+        //std::vector<std::pair<vk::ShaderModule, vk::ShaderStageFlagBits> > shaderModules;
+        std::vector<Shader> shaders;
         void destroy();
     };
 }
--- src/ShaderSet.cpp
+++ src/ShaderSet.cpp
@@ -6,44 +6,48 @@ namespace spk
     {
     }
 
-    ShaderSet::ShaderSet(const ShaderSet& set)
+    //ShaderSet::ShaderSet(const ShaderSet& set)
+    //{
+    //    create(set.infos);
+    //}
+
+    ShaderSet::ShaderSet(const std::vector<std::string>& shaderFilenames/*const std::vector<ShaderInfo>& shaders*/)
     {
-        create(set.infos);
+    //    create(shaders);
+        create(shaderFilenames);
     }
 
-    ShaderSet::ShaderSet(const std::vector<ShaderInfo>& shaders)
-    {
-        create(shaders);
-    }
+    //ShaderSet& ShaderSet::operator=(const ShaderSet& set)
+    //{
+    //    destroy();
+    //    create(set.infos);
+    //    return *this;
+    //}
 
-    ShaderSet& ShaderSet::operator=(const ShaderSet& set)
-    {
-        destroy();
-        create(set.infos);
-        return *this;
-    }
-
-    void ShaderSet::create(const std::vector<ShaderInfo>& shaders)
+    void ShaderSet::create(const std::vector<std::string>& shaderFilenames/*const std::vector<ShaderInfo>& shaders*/)
     {
         const vk::Device& logicalDevice = system::System::getInstance()->getLogicalDevice();
-        infos = shaders;
-        shaderModules.resize(shaders.size());
+ //       infos = shaders;
+//        shaderModules.resize(shaders.size());
+        shaders.resize(shaderFilenames.size());
         shaderStages.resize(shaders.size());
+
         for(int i = 0; i < shaders.size(); ++i)
         {
-            vk::ShaderModuleCreateInfo info;
-            std::vector<char> code = getCode(shaders[i].filename);
-            info.setCodeSize(code.size());
-            info.setPCode(reinterpret_cast<const uint32_t*>(code.data()));
+            //vk::ShaderModuleCreateInfo info;
+            //std::vector<char> code = getCode(shaders[i].filename);
+            //info.setCodeSize(code.size());
+            //info.setPCode(reinterpret_cast<const uint32_t*>(code.data()));
 
-            logicalDevice.createShaderModule(&info, nullptr, &shaderModules[i].first);
+            //logicalDevice.createShaderModule(&info, nullptr, &shaderModules[i].first);
 
-            shaderModules[i].second = shaders[i].type;
+            //shaderModules[i].second = shaders[i].type;
+            shaders[i].load(shaderFilenames[i]);
         }
-        for(int i = 0; i < shaderModules.size(); ++i)
+        for(int i = 0; i < shaders.size(); ++i)
         {
-            shaderStages[i].setModule(shaderModules[i].first);
-            shaderStages[i].setStage(shaderModules[i].second);
+            shaderStages[i].setModule(shaders[i].getShader()/*shaderModules[i].first*/);
+            shaderStages[i].setStage(shaders[i].getType()/*shaderModules[i].second*/);
             shaderStages[i].setPName("main");
             shaderStages[i].setPSpecializationInfo(nullptr);
         }
@@ -54,28 +58,28 @@ namespace spk
         return shaderStages;
     }
 
-    std::vector<char> ShaderSet::getCode(const std::string& filename) const
-    {
-        std::ifstream fin;
-        fin.open(filename, std::ios::ate | std::ios::binary);
-        size_t size = fin.tellg();
-        std::vector<char> code(size);
-        fin.seekg(0);
-        fin.read(code.data(), size);
-        fin.close();
-        return code;
-    }
+    //std::vector<char> ShaderSet::getCode(const std::string& filename) const
+    //{
+    //    std::ifstream fin;
+    //    fin.open(filename, std::ios::ate | std::ios::binary);
+    //    size_t size = fin.tellg();
+    //    std::vector<char> code(size);
+    //    fin.seekg(0);
+    //    fin.read(code.data(), size);
+    //    fin.close();
+    //    return code;
+    //}
 
     void ShaderSet::destroy()
     {
-        if(shaderModules.size() != 0 && shaderModules[0].first)
-        {
-            for(auto& module : shaderModules)
-            {
-                const vk::Device& logicalDevice = system::System::getInstance()->getLogicalDevice();
-                logicalDevice.destroyShaderModule(module.first, nullptr);
-            }
-        }
+//        if(shaderModules.size() != 0 && shaderModules[0].first)
+//        {
+//            for(auto& module : shaderModules)
+//            {
+//                const vk::Device& logicalDevice = system::System::getInstance()->getLogicalDevice();
+//                logicalDevice.destroyShaderModule(module.first, nullptr);
+//            }
+//        }
     }
 
     ShaderSet::~ShaderSet()
