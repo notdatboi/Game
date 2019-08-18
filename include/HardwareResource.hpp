64a16c6014e87a2b4a99bd33972c8b3970cafd3f
notdatboi <noddatboi@gmail.com>
2019-08-18 17:52:18 +0300

    Created base class for hardware resource buffers

---
+++ include/HardwareResource.hpp
@@ -0,0 +1,27 @@
+#ifndef SPARK_HARDWARE_RESOURCE_HPP
+#define SPARK_HARDWARE_RESOURCE_HPP
+#include<SparkIncludeBase.hpp>
+
+namespace spk
+{
+    enum class HardwareResourceAccessibility
+    {
+        Static,                             // Recommended. Assumption that the buffer is changed rarely, changing takes more time, but reading from it is greatly more optimal. Created with 'shadow buffer' to easily retrieve data
+        Dynamic                             // Assumption that the buffer is changed frequently, changing takes almost no time, but usage takes more time
+    };
+
+    class HardwareResource
+    {
+    public:
+        virtual void setShadowBufferPolicy(bool use = false) = 0;
+        virtual void setAccessibility(const HardwareResourceAccessibility accessibility) = 0;
+        virtual void load() = 0;
+        virtual void waitUntilReady() = 0;
+        virtual void resetWaiter() = 0;
+        virtual void clearResources() = 0;
+        virtual ~HardwareResource();
+    protected:
+    };
+}
+
+#endif
