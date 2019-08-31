680ec37bcb36758578ef40c720c2bf3d3d04e160
notdatboi <noddatboi@gmail.com>
2019-08-31 15:58:19 +0300

    Added useful array class and report functions

---
+++ include/Utils.hpp
@@ -0,0 +1,141 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+#include<initializer_list>
+
+template<typename T>
+class Array
+{
+public:
+    Array(): array(nullptr), size(0) {}
+    
+    Array(const std::initializer_list<T>& lst)
+    {
+        create(lst.size());
+        auto listIterator = std::begin(lst);
+        for(unsigned int ind = 0; ind < size; ++ind)
+        {
+            (*this)[ind] = *listIterator;
+            ++listIterator;
+        }
+    }
+    
+    Array(const unsigned int size)
+    {
+        create(size);
+    }
+
+    Array(const Array& other)
+    {
+        *this = other;
+    }
+
+    Array(Array&& other): array(other.array), size(other.size)
+    {
+        other.array = nullptr;
+        other.size = 0;
+    }
+
+    void create(const unsigned int size, const T& defaultElement)
+    {
+        create(size);
+        for(auto ind = 0; ind < size; ++ind)
+        {
+            *(array + ind) = defaultElement;
+        }
+    }
+
+    void create(const unsigned int size = 0)
+    {
+        array = new T[size];
+        this->size = size;
+    }
+
+    void create(const unsigned int size, const T* data)
+    {
+        create(size);
+        for(int ind = 0; ind < size; ++ind)
+        {
+            (*this)[ind] = *(data + ind);
+        }
+    }
+
+    const T& operator[](const unsigned int index) const
+    {
+        return *(array + index);
+    }
+
+    T& operator[](const unsigned int index)
+    {
+        return *(array + index);
+    }
+
+    Array& operator=(const Array& other)
+    {
+        clean();
+        create(other.size);
+        for(unsigned int ind = 0; ind < size; ++ind)
+        {
+            *(array + ind) = other[ind];
+        }
+        return *this;
+    }
+
+    Array& operator=(Array&& other)
+    {
+        clean();
+        array = other.array;
+        size = other.size;
+        other.array = nullptr;
+        other.size = 0;
+        return *this;
+    }
+
+    const bool empty() const
+    {
+        return size == 0;
+    }
+
+    const unsigned int getSize() const
+    {
+        return size;
+    }
+
+    const T* getPtr() const
+    {
+        return array;
+    }
+
+    T* getPtr()
+    {
+        return array;
+    }
+
+    void clean()
+    {
+        if(array)
+        {
+            delete[] array;
+            size = 0;
+        }
+    }
+
+    ~Array()
+    {
+        clean();
+    }
+private:
+    T* array;
+    unsigned int size;
+};
+
+void checkResult(const VkResult& result, const char* error)
+{
+    if(result != VK_SUCCESS) reportError(error);
+}
+
+void reportError(const char* error)
+{
+    throw error;
+}
+
+#endif
