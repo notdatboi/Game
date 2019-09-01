715eca7ac86fd6e8c6030245980a02ad92249ccd
notdatboi <noddatboi@gmail.com>
2019-09-01 19:49:05 +0300

    More checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include<Swapchain.hpp>
+#include<MemoryPool.hpp>
 
 int main()
 {
@@ -8,6 +9,18 @@ int main()
     System system;
     VkPhysicalDeviceFeatures features{};
     system.create(window, true, features);
+    Array<VkMemoryRequirements> rq(1);
+    rq[0] = 
+    {
+        2048,
+        2048,
+        (~0U)
+    };
+    MemoryPool pool;
+    pool.create(&system, 1);
+    pool.allocate(rq, 0);
+    pool[0];
+    pool.destroy();
     Swapchain swapchain;
     swapchain.create(&system);
     swapchain.destroy();
