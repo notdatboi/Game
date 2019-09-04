88d878883494dc97ba48d509d7dd4d3856bc05a5
notdatboi <noddatboi@gmail.com>
2019-09-04 21:08:50 +0300

    Added vertex buffer generators

---
+++ include/MeshLoaderUtils.hpp
@@ -0,0 +1,24 @@
+#ifndef MESH_LOADER_UTILS_HPP
+#define MESH_LOADER_UTILS_HPP
+#include<assimp/scene.h>
+
+class VBGenerator
+{
+public:
+    virtual void generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const = 0;  // texture coord index equal to ~0 must be ignored
+private:
+};
+
+class VBGeneratorOptimal : public VBGenerator   // generates "archived" data (uses free bits left after aligning)
+{
+public:
+    virtual void generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const;
+private:
+    struct Vertex
+    {
+        float posAndU[4];
+        float normalAndV[4];
+    };
+};
+
+#endif
---
+++ src/MeshLoaderUtils.cpp
@@ -0,0 +1,22 @@
+#include<MeshLoaderUtils.hpp>
+#include<Utils.hpp>
+
+void VBGeneratorOptimal::generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex) const
+{
+    if(textureCoordIndex == (~0) || !mesh.HasPositions() || !mesh.HasNormals()) reportError("Invalid mesh.\n");
+    Vertex* dstVertices = (Vertex*)dst;
+    for(auto ind = 0; ind < vertexCount; ++ind)
+    {
+        Vertex tmp;
+        const aiVector3D& pos = *(mesh.mVertices + ind), & uv = *(mesh.mTextureCoords[textureCoordIndex] + ind), & normal = *(mesh.mNormals + ind);
+        tmp.posAndU[0] = pos.x;
+        tmp.posAndU[1] = pos.y;
+        tmp.posAndU[2] = pos.z;
+        tmp.posAndU[3] = uv.x;
+        tmp.normalAndV[0] = normal.x;
+        tmp.normalAndV[1] = normal.y;
+        tmp.normalAndV[2] = normal.z;
+        tmp.normalAndV[3] = uv.y;
+        memcpy(dstVertices + ind, &tmp, sizeof(Vertex));
+    }
+}
