b746c8e7d5c672d2229a8d88e8279de40e08adab
notdatboi <noddatboi@gmail.com>
2019-09-04 14:54:18 +0300

    Edited Shader class to match new System

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -1,33 +1,25 @@
-#ifndef SPARK_SHADER_HPP
-#define SPARK_SHADER_HPP
+#ifndef SHADER_HPP
+#define SHADER_HPP
 
-#include<string>
 #include<glslang/Public/ShaderLang.h>
 #include<SPIRV/GlslangToSpv.h>
 #include<Glslang/DirStackFileIncluder.h>
-#include<fstream>
-#include<iostream>
+#include<System.hpp>
 
-namespace spk
+class Shader
 {
-    class Shader
-    {
-    public:
-        void load(const std::string& filename);
-        const vk::ShaderModule& getShader() const;
-        const vk::ShaderStageFlagBits getType() const;
-        const bool operator==(const Shader& other) const;
-        const bool operator!=(const Shader& other) const;
-        ~Shader();
-    private:
-        const std::string getFilePath(const std::string& filename);
-        const std::string getSuffix(const std::string& filename);
-        const EShLanguage parseAndSetType(const std::string& suffix);
-        static const int clientInputSematicsVersion = 100;
-        static const glslang::EshTargetClientVersion vulkanClientVersion = glslang::EShTargetVulkan_1_0;
-        static const glslang::EShTargetLanguageVersion targetLangVersion = glslang::EShTargetSpv_1_0;
-        vk::ShaderModule shader;
-        vk::ShaderStageFlagBits type;
-    };
-}
+public:
+    Shader();
+    void create(const System* system, const char* filename);
+    const ShaderStageInfo& getShader() const;
+    void destroy();
+    ~Shader();
+private:
+    static const int clientInputSematicsVersion = 100;
+    static const glslang::EshTargetClientVersion vulkanClientVersion = glslang::EShTargetVulkan_1_0;
+    static const glslang::EShTargetLanguageVersion targetLangVersion = glslang::EShTargetSpv_1_0;
+    const System* system;
+    ShaderStageInfo shader;
+};
+
 #endif
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,156 +1,142 @@
 #include<Shader.hpp>
+#include<iostream>
+#include<string>
 
-namespace spk
+const std::string getFilePath(const std::string& filename)
 {
-    void Shader::load(const std::string& filename)
+    const auto folderNameEndIndex = filename.find_last_of("/\\");
+    return filename.substr(0, folderNameEndIndex);
+}
+
+const std::string getSuffix(const std::string& filename)
+{
+    const auto dotPos = filename.rfind('.');
+    return (dotPos != std::string::npos) ? filename.substr(dotPos + 1) : "";
+}
+
+const EShLanguage parseAndSetType(const std::string& suffix, VkShaderStageFlagBits& type)
+{
+    if(suffix == "vert")
     {
-        std::vector<uint32_t> compiled;
+        type = VkShaderStageFlagBits::VK_SHADER_STAGE_VERTEX_BIT;
+        return EShLangVertex;
+    }
+    else if(suffix == "tesc")
+    {
+        type = VkShaderStageFlagBits::VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT;
+        return EShLangTessControl;
+    }
+    else if(suffix == "tese")
+    {
+        type = VkShaderStageFlagBits::VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT;
+        return EShLangTessEvaluation;
+    }
+    else if(suffix == "geom")
+    {
+        type = VkShaderStageFlagBits::VK_SHADER_STAGE_GEOMETRY_BIT;
+        return EShLangGeometry;
+    }
+    else if(suffix == "frag")
+    {
+        type = VkShaderStageFlagBits::VK_SHADER_STAGE_FRAGMENT_BIT;
+        return EShLangFragment;
+    }
+    else if(suffix == "comp")
+    {
+        type = VkShaderStageFlagBits::VK_SHADER_STAGE_COMPUTE_BIT;
+        return EShLangCompute;
+    }
+    else
+    {
+        std::cout << "Invalid shader.\n";
+    }
+}
 
-        glslang::InitializeProcess();
-        
-        std::ifstream file(filename);
+void Shader::create(const System* system, const char* filename)
+{
+    this->system = system;
 
-        if(!file.is_open())
-        {
-            glslang::FinalizeProcess();
-            throw std::runtime_error("Shader file not found.");
-        }
+    std::vector<uint32_t> compiled;
 
-        std::string glslInput{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
-        const char* glslInputCStr = glslInput.c_str();
+    glslang::InitializeProcess();
+    
+    std::ifstream file(filename);
 
-        EShLanguage shaderType = parseAndSetType(getSuffix(filename));
-        glslang::TShader glslShader(shaderType);
-        glslShader.setStrings(&glslInputCStr, 1);
-        glslShader.setEnvInput(glslang::EShSourceGlsl, shaderType, glslang::EShClientVulkan, clientInputSematicsVersion);
-        glslShader.setEnvClient(glslang::EShClientVulkan, vulkanClientVersion);
-        glslShader.setEnvTarget(glslang::EshTargetSpv, targetLangVersion);
-
-        const EShMessages messages = (EShMessages)(EShMsgSpvRules | EShMsgVulkanRules);
-
-        DirStackFileIncluder includer;
-        includer.pushExternalLocalDirectory(getFilePath(filename));
-
-        std::string glslPreprocessed;
-        if(!glslShader.preprocess(&DefaultTBuiltInResource, clientInputSematicsVersion, ENoProfile, false, false, messages, &glslPreprocessed, includer))
-        {
-            std::cout << "GLSL Preprocessing Failed for: " << filename << std::endl;
-            std::cout << glslShader.getInfoLog() << std::endl;
-            std::cout << glslShader.getInfoDebugLog() << std::endl;
-        }
-
-        const char* preprocessedCStr = glslPreprocessed.c_str();
-        glslShader.setStrings(&preprocessedCStr, 1);
-
-        if (!glslShader.parse(&DefaultTBuiltInResource, clientInputSematicsVersion, false, messages))
-        {
-            std::cout << "GLSL Parsing Failed for: " << filename << std::endl;
-            std::cout << glslShader.getInfoLog() << std::endl;
-            std::cout << glslShader.getInfoDebugLog() << std::endl;
-        }
-
-        glslang::TProgram program;
-        program.addShader(&glslShader);
-        if(!program.link(messages))
-        {
-            std::cout << "GLSL Linking Failed for: " << filename << std::endl;
-            std::cout << glslShader.getInfoLog() << std::endl;
-            std::cout << glslShader.getInfoDebugLog() << std::endl;
-        }
-
-        spv::SpvBuildLogger logger;
-        glslang::SpvOptions spvOptions;
-        glslang::GlslangToSpv(*program.getIntermediate(shaderType), compiled, &logger, &spvOptions);
-
-        vk::ShaderModuleCreateInfo info;
-        info.setCodeSize(compiled.size() * sizeof(uint32_t))
-            .setPCode(compiled.data());
-
-        const auto& logicalDevice = system::System::getInstance()->getLogicalDevice();
-        if(logicalDevice.createShaderModule(&info, nullptr, &shader) != vk::Result::eSuccess)
-        {
-            glslang::FinalizeProcess();
-            throw std::runtime_error("Failed to create shader module!\n");
-        }
+    if(!file.is_open())
+    {
         glslang::FinalizeProcess();
+        throw std::runtime_error("Shader file not found.");
     }
 
-    const std::string Shader::getFilePath(const std::string& filename)
+    std::string glslInput{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+    const char* glslInputCStr = glslInput.c_str();
+
+    EShLanguage shaderType = parseAndSetType(std::move(getSuffix(filename)), shader.stage);
+    glslang::TShader glslShader(shaderType);
+    glslShader.setStrings(&glslInputCStr, 1);
+    glslShader.setEnvInput(glslang::EShSourceGlsl, shaderType, glslang::EShClientVulkan, clientInputSematicsVersion);
+    glslShader.setEnvClient(glslang::EShClientVulkan, vulkanClientVersion);
+    glslShader.setEnvTarget(glslang::EshTargetSpv, targetLangVersion);
+
+    const EShMessages messages = (EShMessages)(EShMsgSpvRules | EShMsgVulkanRules);
+
+    DirStackFileIncluder includer;
+    includer.pushExternalLocalDirectory(getFilePath(filename));
+
+    std::string glslPreprocessed;
+    if(!glslShader.preprocess(&DefaultTBuiltInResource, clientInputSematicsVersion, ENoProfile, false, false, messages, &glslPreprocessed, includer))
     {
-        const auto folderNameEndIndex = filename.find_last_of("/\\");
-        return filename.substr(0, folderNameEndIndex);
+        std::cout << "GLSL Preprocessing Failed for: " << filename << std::endl;
+        std::cout << glslShader.getInfoLog() << std::endl;
+        std::cout << glslShader.getInfoDebugLog() << std::endl;
     }
 
-    const std::string Shader::getSuffix(const std::string& filename)
+    const char* preprocessedCStr = glslPreprocessed.c_str();
+    glslShader.setStrings(&preprocessedCStr, 1);
+
+    if (!glslShader.parse(&DefaultTBuiltInResource, clientInputSematicsVersion, false, messages))
     {
-        const auto dotPos = filename.rfind('.');
-        return (dotPos != std::string::npos) ? filename.substr(dotPos + 1) : "";
+        std::cout << "GLSL Parsing Failed for: " << filename << std::endl;
+        std::cout << glslShader.getInfoLog() << std::endl;
+        std::cout << glslShader.getInfoDebugLog() << std::endl;
     }
 
-    const EShLanguage Shader::parseAndSetType(const std::string& suffix)
+    glslang::TProgram program;
+    program.addShader(&glslShader);
+    if(!program.link(messages))
     {
-        if(suffix == "vert")
-        {
-            type = vk::ShaderStageFlagBits::eVertex;
-            return EShLangVertex;
-        }
-        else if(suffix == "tesc")
-        {
-            type = vk::ShaderStageFlagBits::eTessellationControl;
-            return EShLangTessControl;
-        }
-        else if(suffix == "tese")
-        {
-            type = vk::ShaderStageFlagBits::eTessellationEvaluation;
-            return EShLangTessEvaluation;
-        }
-        else if(suffix == "geom")
-        {
-            type = vk::ShaderStageFlagBits::eGeometry;
-            return EShLangGeometry;
-        }
-        else if(suffix == "frag")
-        {
-            type = vk::ShaderStageFlagBits::eFragment;
-            return EShLangFragment;
-        }
-        else if(suffix == "comp")
-        {
-            type = vk::ShaderStageFlagBits::eCompute;
-            return EShLangCompute;
-        }
-        else
-        {
-            throw std::invalid_argument("Invalid shader suffix given.\n");
-        }
-    }
-    const bool Shader::operator==(const Shader& other) const
-    {
-        return shader == other.shader;
+        std::cout << "GLSL Linking Failed for: " << filename << std::endl;
+        std::cout << glslShader.getInfoLog() << std::endl;
+        std::cout << glslShader.getInfoDebugLog() << std::endl;
     }
 
-    const bool Shader::operator!=(const Shader& other) const
-    {
-        return !((*this) == other);
-    }
+    spv::SpvBuildLogger logger;
+    glslang::SpvOptions spvOptions;
+    glslang::GlslangToSpv(*program.getIntermediate(shaderType), compiled, &logger, &spvOptions);
 
-    const vk::ShaderModule& Shader::getShader() const
-    {
-        return shader;
-    }
+    VkShaderModuleCreateInfo info;
+    info.codeSize = compiled.size() * sizeof(uint32_t);
+    info.pCode = compiled.data();
 
-    const vk::ShaderStageFlagBits Shader::getType() const
-    {
-        return type;
-    }
+    checkResult(vkCreateShaderModule(system->getDevice(), &info, nullptr, &shader.module), "Failed to create shader.\n");
+    glslang::FinalizeProcess();
+}
 
-    Shader::~Shader()
+const ShaderStageInfo& Shader::getShader() const
+{
+    return shader;
+}
+
+void Shader::destroy()
+{
+    if(shader.module)
     {
-        const auto& logicalDevice = system::System::getInstance()->getLogicalDevice();
-        if(shader)
-        {
-            logicalDevice.destroyShaderModule(shader, nullptr);
-            shader = vk::ShaderModule();
-        }
+        vkDestroyShaderModule(system->getDevice(), shader.module, nullptr);
+        shader.module = 0;
     }
+}
+
+Shader::~Shader()
+{
+    destroy();
 }
