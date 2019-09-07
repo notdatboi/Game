b7b6ddec38a9231a09bc858ea9b0d7267ce651cb
notdatboi <noddatboi@gmail.com>
2019-09-07 20:41:28 +0300

    Added constants

---
+++ include/Constants.hpp
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+enum Semaphores{Count};
+enum Fences{Count};
+enum MemoryObjects{Count};
+enum Buffers{Count};
+enum Images{Count};
+enum Pipelines{Count};
+enum PipelineLayouts{Count};
+enum DescriptorSetLayouts{Count};
+enum DescriptorSets{Count};
+
+#endif
