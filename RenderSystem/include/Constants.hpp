0d09b83f4ffda59c9b2e93bbf5076ac9fcd1e58d
notdatboi <noddatboi@gmail.com>
2019-09-10 16:42:07 +0300

    Populated some enums

--- RenderSystem/include/Constants.hpp
+++ RenderSystem/include/Constants.hpp
@@ -1,11 +1,12 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-enum Semaphores{ImageAcquired1, ImageRendered1, ImageAcquired2, ImageRendered2, ImageAcquired3, ImageRendered3, Count};
-enum Fences{ImageAcquired1, ImageRendered1, ImageAcquired2, ImageRendered2, ImageAcquired3, ImageRendered3, Count};
-enum MemoryObjects{Transfer, VertexIndex, Uniform, Image, Count};
+enum Semaphores{ImageAcquired1, ImageRendered1, ImageAcquired2, ImageRendered2, ImageAcquired3, ImageRendered3, TransferFilished, Count};
+enum Fences{ImageAcquired1, ImageRendered1, ImageAcquired2, ImageRendered2, ImageAcquired3, ImageRendered3, TransferSubmitted, Count};
+enum MemoryObjects{Transfer, VertexIndex, Uniform, Image, DepthAttachment, Count};
 enum Buffers{Transfer, VertexIndex, Uniform, Count};
-enum Images{DepthAttachment1, DepthAttachment2, DepthAttachment3, Count};
+enum DepthImages{DepthAttachment1, DepthAttachment2, DepthAttachment3, Count};
+enum CommandBuffers{Render1, Render2, Render3, Transfer, Count};
 enum Pipelines{Count};
 enum PipelineLayouts{Count};
 enum DescriptorSetLayouts{Count};
