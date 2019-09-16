723fae30f53585d136399bc94b7d531939c23cc6
notdatboi <noddatboi@gmail.com>
2019-09-16 11:25:48 +0300

    Changed loader-related constants to actual global constants

--- RenderSystem/include/Constants.hpp
+++ RenderSystem/include/Constants.hpp
@@ -1,15 +1,8 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-enum Semaphores{ImageAcquired1, ImageRendered1, ImageAcquired2, ImageRendered2, ImageAcquired3, ImageRendered3, TransferFilished, Count};
-enum Fences{ImageAcquired1, ImageRendered1, ImageAcquired2, ImageRendered2, ImageAcquired3, ImageRendered3, TransferSubmitted, Count};
-enum MemoryObjects{Transfer, VertexIndex, Uniform, Image, DepthAttachment, Count};
-enum Buffers{Transfer, VertexIndex, Uniform, Count};
-enum DepthImages{DepthAttachment1, DepthAttachment2, DepthAttachment3, Count};
-enum CommandBuffers{Render1, Render2, Render3, Transfer, Count};
-enum Pipelines{Count};
-enum PipelineLayouts{Count};
-enum DescriptorSetLayouts{Count};
-enum DescriptorSets{Count};
+#define MAX_VERTEX_COUNT 100000
+#define MAX_VERTEX_SIZE 24
+#define MAX_IMAGE_DIMENSION 1024
 
 #endif
