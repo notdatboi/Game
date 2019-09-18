b56028bdcf902ea4c9b90e52f47fbcbe3462f8a7
notdatboi <noddatboi@gmail.com>
2019-09-18 22:11:34 +0300

    Added Image class, which can be loaded by itself

--- RenderSystem/include/ImageLoader.hpp
+++ RenderSystem/include/ImageLoader.hpp
@@ -1,16 +1,38 @@
 #ifndef IMAGE_LOADER_HPP
 #define IMAGE_LOADER_HPP
+#include<Utils.hpp>
 
 class ImageLoader
 {
 public:
+    class Image
+    {
+    public:
+        Image();
+        void load(const char* filename, int forcedChannels = 0);        // 0 channels -> auto-check
+        Image& operator=(Image&& img);
+        Image& operator=(const Image& img) = delete;
+        Image& operator=(Image& img) = delete;
+        Image& operator=(Image img) = delete;
+        const unsigned char* getData() const;
+        const VkExtent2D getExtent() const;
+        const uint32_t getChannelCount() const;
+        void unload();
+        ~Image();
+    private:
+        int width;
+        int height;
+        int channels;
+        unsigned char* data;
+    };
     ImageLoader();
-    void load(const char* filename, int& width, int& height, int& channelCount);        // 0 channels -> auto-check
-    const unsigned char* getData() const;
-    void unload();
+    void create(const uint32_t maxImageCount);
+    Image& operator[](const uint32_t index);
+    const Image& operator[](const uint32_t index) const;
     ~ImageLoader();
 private:
-    unsigned char* data;
+    //unsigned char* data;
+    Array<Image> images;
 };
 
 #endif
--- RenderSystem/src/ImageLoader.cpp
+++ RenderSystem/src/ImageLoader.cpp
@@ -2,23 +2,64 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include<External/Stb/stb_image.h>
 
-ImageLoader::ImageLoader(): data(nullptr)
+ImageLoader::ImageLoader()
 {
 }
 
-void ImageLoader::load(const char* filename, int& width, int& height, int& channelCount)
+void ImageLoader::create(const uint32_t maxImageCount)
 {
-    int channels = channelCount;
-    data = stbi_load(filename, &width, &height, &channels, channelCount);
-    if(channelCount == 0) channelCount = channels;
+    images.create(maxImageCount);
 }
 
-const unsigned char* ImageLoader::getData() const
+ImageLoader::Image& ImageLoader::operator[](const uint32_t index)
+{
+    return images[index];
+}
+
+const ImageLoader::Image& ImageLoader::operator[](const uint32_t index) const
+{
+    return images[index];
+}
+
+ImageLoader::~ImageLoader()
+{
+}
+
+
+ImageLoader::Image::Image(){}
+
+void ImageLoader::Image::load(const char* filename, int forcedChannels)
+{
+    data = stbi_load(filename, &width, &height, &channels, forcedChannels);
+    if(forcedChannels != 0) channels = forcedChannels;
+}
+
+ImageLoader::Image& ImageLoader::Image::operator=(Image&& img)
+{
+    width = img.width;
+    height = img.height;
+    channels = img.channels;
+    data = img.data;
+    img.data = nullptr;
+    return *this;
+}
+
+const unsigned char* ImageLoader::Image::getData() const
 {
     return data;
 }
 
-void ImageLoader::unload()
+const VkExtent2D ImageLoader::Image::getExtent() const
+{
+    return {width, height};
+}
+
+const uint32_t ImageLoader::Image::getChannelCount() const
+{
+    return channels;
+}
+
+void ImageLoader::Image::unload()
 {
     if(data)
     {
@@ -27,7 +68,7 @@ void ImageLoader::unload()
     }
 }
 
-ImageLoader::~ImageLoader()
+ImageLoader::Image::~Image()
 {
     unload();
 }
