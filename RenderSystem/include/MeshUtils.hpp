9cb8edd82e2787089fb8ea9f2cc7804223bd6583
notdatboi <noddatboi@gmail.com>
2019-09-23 12:21:17 +0300

    Removed vertex buffer allocators, added vertex buffers instead

--- RenderSystem/include/MeshUtils.hpp
+++ RenderSystem/include/MeshUtils.hpp
@@ -1,21 +1,36 @@
 #ifndef MESH_UTILS_HPP
 #define MESH_UTILS_HPP
 #include<assimp/scene.h>
+#include<Utils.hpp>
 
-class VBGenerator
+class VertexBuffer
 {
 public:
-    virtual void generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const = 0;  // texture coord index equal to ~0 must be ignored
-    virtual const uint32_t getVertexSize() const = 0;
-private:
+    virtual const bool checkCompatibility(const aiMesh* mesh) const = 0;
+    virtual void loadFromAiMesh(const aiMesh* mesh) = 0;
+    virtual void* getBufferPtr() = 0;
+    virtual const uint32_t getBufferSize() const = 0;
+    virtual const uint32_t getVertexCount() const = 0;
+    virtual void getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes) = 0;
+    virtual void clear() = 0;
+    static const uint32_t getFirstTextureCoordIndex(const aiMesh* mesh);
+    static const bool hasPositions(const aiMesh* mesh);
+    static const bool hasNormals(const aiMesh* mesh);
+    static const bool hasTangentsAndBitangents(const aiMesh* mesh);
+protected:
 };
 
-class VBGeneratorStandard : public VBGenerator   // generates "archived" data (uses free bits left after aligning)
+class VertexBufferStandard : public VertexBuffer
 {
 public:
-    virtual void generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const;
-    virtual const uint32_t getVertexSize() const;
-private:
+    virtual const bool checkCompatibility(const aiMesh* mesh) const;
+    virtual void loadFromAiMesh(const aiMesh* mesh);
+    virtual void* getBufferPtr();
+    virtual const uint32_t getBufferSize() const;
+    virtual const uint32_t getVertexCount() const;
+    virtual void getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes);
+    virtual void clear();
+protected:
     struct Vertex
     {
         float posAndU[4];
@@ -23,34 +38,47 @@ private:
         float tan[4];
         float btan[4];
     };
+    Array<Vertex> vertices;
 };
 
-class VBGeneratorNotTextured : public VBGenerator
+class VertexBufferNotTextured : public VertexBuffer
 {
 public:
-    virtual void generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const;
-    virtual const uint32_t getVertexSize() const;
-private:
+    virtual const bool checkCompatibility(const aiMesh* mesh) const;
+    virtual void loadFromAiMesh(const aiMesh* mesh);
+    virtual void* getBufferPtr();
+    virtual const uint32_t getBufferSize() const;
+    virtual const uint32_t getVertexCount() const;
+    virtual void getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes);
+    virtual void clear();
+protected:
     struct Vertex
     {
         float posAndNormX[4];
         float tanAndNormY[4];
         float btanAndNormZ[4];
     };
+    Array<Vertex> vertices;
 };
 
-class VBGeneratorWithNormalMap : public VBGenerator
+class VertexBufferWithNormalMap : public VertexBuffer
 {
 public:
-    virtual void generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const;
-    virtual const uint32_t getVertexSize() const;
-private:
+    virtual const bool checkCompatibility(const aiMesh* mesh) const;
+    virtual void loadFromAiMesh(const aiMesh* mesh);
+    virtual void* getBufferPtr();
+    virtual const uint32_t getBufferSize() const;
+    virtual const uint32_t getVertexCount() const;
+    virtual void getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes);
+    virtual void clear();
+protected:
     struct Vertex
     {
         float pos[4];
         float tanAndU[4];
         float btanAndV[4];
     };
+    Array<Vertex> vertices;
 };
 
 #endif
--- RenderSystem/src/MeshUtils.cpp
+++ RenderSystem/src/MeshUtils.cpp
@@ -1,93 +1,295 @@
 #include<MeshUtils.hpp>
-#include<Utils.hpp>
 
-void VBGeneratorStandard::generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex) const
+const uint32_t VertexBuffer::getFirstTextureCoordIndex(const aiMesh* mesh)
 {
-    if(textureCoordIndex == (~0) || !mesh.HasPositions() || !mesh.HasNormals() || !mesh.HasTangentsAndBitangents()) reportError("Invalid mesh.\n");
-    Vertex* dstVertices = (Vertex*)dst;
-    for(auto ind = 0; ind < vertexCount; ++ind)
+    for(auto index = 0; index < AI_MAX_NUMBER_OF_TEXTURECOORDS; ++index)
     {
-        Vertex tmp;
-        const aiVector3D* pos = mesh.mVertices + ind, * uv = mesh.mTextureCoords[textureCoordIndex] + ind, * normal = mesh.mNormals + ind, * tan = mesh.mTangents + ind, * btan = mesh.mBitangents + ind;
-        tmp.posAndU[0] = pos->x;
-        tmp.posAndU[1] = pos->y;
-        tmp.posAndU[2] = pos->z;
-        tmp.posAndU[3] = uv->x;
-        tmp.normalAndV[0] = normal->x;
-        tmp.normalAndV[1] = normal->y;
-        tmp.normalAndV[2] = normal->z;
-        tmp.normalAndV[3] = uv->y;
-        tmp.tan[0] = tan->x;
-        tmp.tan[1] = tan->y;
-        tmp.tan[2] = tan->z;
-        tmp.btan[0] = btan->x;
-        tmp.btan[1] = btan->y;
-        tmp.btan[2] = btan->z;
+        if(mesh->HasTextureCoords(index))
+        {
+            return index;
+            break;
+        }
+    }
+    return ~0;
+}
 
-        memcpy(dstVertices + ind, &tmp, sizeof(Vertex));
+const bool VertexBuffer::hasPositions(const aiMesh* mesh)
+{
+    return mesh->HasPositions();
+}
+
+const bool VertexBuffer::hasNormals(const aiMesh* mesh)
+{
+    return mesh->HasNormals();
+}
+
+const bool VertexBuffer::hasTangentsAndBitangents(const aiMesh* mesh)
+{
+    return mesh->HasTangentsAndBitangents();
+}
+
+//standard
+
+const bool VertexBufferStandard::checkCompatibility(const aiMesh* mesh) const
+{
+    if(VertexBuffer::hasPositions(mesh)
+        && VertexBuffer::hasNormals(mesh)
+        && VertexBuffer::hasTangentsAndBitangents(mesh)
+        && VertexBuffer::getFirstTextureCoordIndex(mesh) != (~0)) 
+            return true;
+    return false;
+}
+
+void VertexBufferStandard::loadFromAiMesh(const aiMesh* mesh)
+{
+    vertices.create(mesh->mNumVertices);
+    auto textureCoordIndex = getFirstTextureCoordIndex(mesh);
+    for(auto ind = 0; ind < vertices.getSize(); ++ind)
+    {
+        const aiVector3D* pos = mesh->mVertices + ind, * uv = mesh->mTextureCoords[textureCoordIndex] + ind, * normal = mesh->mNormals + ind, * tan = mesh->mTangents + ind, * btan = mesh->mBitangents + ind;
+        vertices[ind].posAndU[0] = pos->x;
+        vertices[ind].posAndU[1] = pos->y;
+        vertices[ind].posAndU[2] = pos->z;
+        vertices[ind].posAndU[3] = uv->x;
+        vertices[ind].normalAndV[0] = normal->x;
+        vertices[ind].normalAndV[1] = normal->y;
+        vertices[ind].normalAndV[2] = normal->z;
+        vertices[ind].normalAndV[3] = uv->y;
+        vertices[ind].tan[0] = tan->x;
+        vertices[ind].tan[1] = tan->y;
+        vertices[ind].tan[2] = tan->z;
+        vertices[ind].btan[0] = btan->x;
+        vertices[ind].btan[1] = btan->y;
+        vertices[ind].btan[2] = btan->z;
     }
 }
 
-const uint32_t VBGeneratorStandard::getVertexSize() const
+void* VertexBufferStandard::getBufferPtr()
 {
-    return sizeof(Vertex);
+    return vertices.getPtr();
 }
 
-void VBGeneratorNotTextured::generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const
+const uint32_t VertexBufferStandard::getBufferSize() const
 {
-    if(!mesh.HasPositions() || !mesh.HasNormals() || !mesh.HasTangentsAndBitangents()) reportError("Invalid mesh.\n");
-    Vertex* dstVertices = (Vertex*)dst;
-    for(auto ind = 0; ind < vertexCount; ++ind)
-    {
-        Vertex tmp;
-        const aiVector3D* pos = mesh.mVertices + ind, * normal = mesh.mNormals + ind, * tan = mesh.mTangents + ind, * btan = mesh.mBitangents + ind;
-        tmp.posAndNormX[0] = pos->x;
-        tmp.posAndNormX[1] = pos->y;
-        tmp.posAndNormX[2] = pos->z;
-        tmp.posAndNormX[3] = normal->x;
-        tmp.tanAndNormY[0] = tan->x;
-        tmp.tanAndNormY[1] = tan->y;
-        tmp.tanAndNormY[2] = tan->z;
-        tmp.tanAndNormY[3] = normal->y;
-        tmp.btanAndNormZ[0] = btan->x;
-        tmp.btanAndNormZ[1] = btan->y;
-        tmp.btanAndNormZ[2] = btan->z;
-        tmp.btanAndNormZ[3] = normal->z;
+    return sizeof(Vertex) * vertices.getSize();
+}
 
-        memcpy(dstVertices + ind, &tmp, sizeof(Vertex));
+const uint32_t VertexBufferStandard::getVertexCount() const
+{
+    return vertices.getSize();
+}
+
+void VertexBufferStandard::getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes)
+{
+    bindings.create(1);
+    bindings[0] = 
+    {
+        0,
+        sizeof(Vertex),
+        VkVertexInputRate::VK_VERTEX_INPUT_RATE_VERTEX
+    };
+    attributes.create(4);
+    attributes[0] = 
+    {
+        0,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, posAndU)
+    };
+    attributes[1] = 
+    {
+        1,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, normalAndV)
+    };
+    attributes[2] = 
+    {
+        2,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, tan)
+    };
+    attributes[3] = 
+    {
+        3,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, btan)
+    };
+}
+
+void VertexBufferStandard::clear()
+{
+    vertices.clear();
+}
+
+//not textured
+
+const bool VertexBufferNotTextured::checkCompatibility(const aiMesh* mesh) const
+{
+    if(VertexBuffer::hasPositions(mesh)
+        && VertexBuffer::hasNormals(mesh)
+        && VertexBuffer::hasTangentsAndBitangents(mesh)) 
+            return true;
+    return false;
+}
+
+void VertexBufferNotTextured::loadFromAiMesh(const aiMesh* mesh)
+{
+    vertices.create(mesh->mNumVertices);
+    auto textureCoordIndex = getFirstTextureCoordIndex(mesh);
+    for(auto ind = 0; ind < vertices.getSize(); ++ind)
+    {
+        const aiVector3D* pos = mesh->mVertices + ind, * normal = mesh->mNormals + ind, * tan = mesh->mTangents + ind, * btan = mesh->mBitangents + ind;
+        vertices[ind].posAndNormX[0] = pos->x;
+        vertices[ind].posAndNormX[1] = pos->y;
+        vertices[ind].posAndNormX[2] = pos->z;
+        vertices[ind].posAndNormX[3] = normal->x;
+        vertices[ind].tanAndNormY[0] = tan->x;
+        vertices[ind].tanAndNormY[1] = tan->y;
+        vertices[ind].tanAndNormY[2] = tan->z;
+        vertices[ind].tanAndNormY[3] = normal->y;
+        vertices[ind].btanAndNormZ[0] = btan->x;
+        vertices[ind].btanAndNormZ[1] = btan->y;
+        vertices[ind].btanAndNormZ[2] = btan->z;
+        vertices[ind].btanAndNormZ[3] = normal->z;
     }
 }
 
-const uint32_t VBGeneratorNotTextured::getVertexSize() const
+void* VertexBufferNotTextured::getBufferPtr()
 {
-    return sizeof(Vertex);
+    return vertices.getPtr();
 }
 
-void VBGeneratorWithNormalMap::generateVertexBuffer(const aiMesh& mesh, void* dst, const uint32_t vertexCount, const uint32_t textureCoordIndex = ~0) const
+const uint32_t VertexBufferNotTextured::getBufferSize() const
 {
-    if(textureCoordIndex == (~0) || !mesh.HasPositions() || !mesh.HasTangentsAndBitangents()) reportError("Invalid mesh.\n");
-    Vertex* dstVertices = (Vertex*)dst;
-    for(auto ind = 0; ind < vertexCount; ++ind)
-    {
-        Vertex tmp;
-        const aiVector3D* pos = mesh.mVertices + ind, * uv = mesh.mTextureCoords[textureCoordIndex] + ind, * tan = mesh.mTangents + ind, * btan = mesh.mBitangents + ind;
-        tmp.pos[0] = pos->x;
-        tmp.pos[1] = pos->y;
-        tmp.pos[2] = pos->z;
-        tmp.tanAndU[0] = tan->x;
-        tmp.tanAndU[1] = tan->y;
-        tmp.tanAndU[2] = tan->z;
-        tmp.tanAndU[3] = uv->x;
-        tmp.btanAndV[0] = btan->x;
-        tmp.btanAndV[1] = btan->y;
-        tmp.btanAndV[2] = btan->z;
-        tmp.btanAndV[3] = uv->y;
+    return sizeof(Vertex) * vertices.getSize();
+}
 
-        memcpy(dstVertices + ind, &tmp, sizeof(Vertex));
+const uint32_t VertexBufferNotTextured::getVertexCount() const
+{
+    return vertices.getSize();
+}
+
+void VertexBufferNotTextured::getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes)
+{
+    bindings.create(1);
+    bindings[0] = 
+    {
+        0,
+        sizeof(Vertex),
+        VkVertexInputRate::VK_VERTEX_INPUT_RATE_VERTEX
+    };
+    attributes.create(3);
+    attributes[0] = 
+    {
+        0,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, posAndNormX)
+    };
+    attributes[1] = 
+    {
+        1,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, tanAndNormY)
+    };
+    attributes[2] = 
+    {
+        2,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, btanAndNormZ)
+    };
+}
+
+void VertexBufferNotTextured::clear()
+{
+    vertices.clear();
+}
+
+//normal mapped
+
+const bool VertexBufferWithNormalMap::checkCompatibility(const aiMesh* mesh) const
+{
+    if(VertexBuffer::hasPositions(mesh)
+        && VertexBuffer::hasTangentsAndBitangents(mesh)
+        && VertexBuffer::getFirstTextureCoordIndex(mesh) != (~0)) 
+            return true;
+    return false;
+}
+
+void VertexBufferWithNormalMap::loadFromAiMesh(const aiMesh* mesh)
+{
+    vertices.create(mesh->mNumVertices);
+    auto textureCoordIndex = getFirstTextureCoordIndex(mesh);
+    for(auto ind = 0; ind < vertices.getSize(); ++ind)
+    {
+        const aiVector3D* pos = mesh->mVertices + ind, * uv = mesh->mTextureCoords[textureCoordIndex] + ind, * tan = mesh->mTangents + ind, * btan = mesh->mBitangents + ind;
+        vertices[ind].pos[0] = pos->x;
+        vertices[ind].pos[1] = pos->y;
+        vertices[ind].pos[2] = pos->z;
+        vertices[ind].tanAndU[0] = tan->x;
+        vertices[ind].tanAndU[1] = tan->y;
+        vertices[ind].tanAndU[2] = tan->z;
+        vertices[ind].tanAndU[3] = uv->x;
+        vertices[ind].btanAndV[0] = btan->x;
+        vertices[ind].btanAndV[1] = btan->y;
+        vertices[ind].btanAndV[2] = btan->z;
+        vertices[ind].btanAndV[3] = uv->y;
     }
 }
 
-const uint32_t VBGeneratorWithNormalMap::getVertexSize() const
+void* VertexBufferWithNormalMap::getBufferPtr()
 {
-    return sizeof(Vertex);
+    return vertices.getPtr();
+}
+const uint32_t VertexBufferWithNormalMap::getBufferSize() const
+{
+    return sizeof(Vertex) * vertices.getSize();
+}
+
+const uint32_t VertexBufferWithNormalMap::getVertexCount() const
+{
+    return vertices.getSize();
+}
+
+void VertexBufferWithNormalMap::getGraphicsPipelineVertexInputState(Array<VkVertexInputBindingDescription>& bindings, Array<VkVertexInputAttributeDescription>& attributes)
+{
+    bindings.create(1);
+    bindings[0] = 
+    {
+        0,
+        sizeof(Vertex),
+        VkVertexInputRate::VK_VERTEX_INPUT_RATE_VERTEX
+    };
+    attributes.create(3);
+    attributes[0] = 
+    {
+        0,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, pos)
+    };
+    attributes[1] = 
+    {
+        1,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, tanAndU)
+    };
+    attributes[2] = 
+    {
+        2,
+        0,
+        VkFormat::VK_FORMAT_R32G32B32A32_SFLOAT,
+        offsetof(Vertex, btanAndV)
+    };
+}
+
+void VertexBufferWithNormalMap::clear()
+{
+    vertices.clear();
 }
