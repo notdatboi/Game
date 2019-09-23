25e4ea45a78159337c15a95df56fe3863e258d81
notdatboi <noddatboi@gmail.com>
2019-09-23 12:14:29 +0300

    Added constants

--- RenderSystem/include/Constants.hpp
+++ RenderSystem/include/Constants.hpp
@@ -4,5 +4,15 @@
 #define MAX_VERTEX_COUNT 100000
 #define MAX_VERTEX_SIZE 24
 #define MAX_IMAGE_DIMENSION 1024
+#define MAX_TEXTURE_COUNT 10
+#define MAX_UNIFORM_COUNT 15
+
+enum DrawableType
+{
+    NotTextured,
+    Textured,
+    TexturedWithNormalMap,
+    Count
+};
 
 #endif
