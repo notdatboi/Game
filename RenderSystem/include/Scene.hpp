6a13225ce31193cc0e3cc0bba0b4da839661214c
notdatboi <noddatboi@gmail.com>
2019-09-29 21:38:55 +0300

    Changed according to allocator

--- RenderSystem/include/Scene.hpp
+++ RenderSystem/include/Scene.hpp
@@ -1,6 +1,7 @@
 #ifndef SCENE_HPP
 #define SCENE_HPP
 #include<assimp/Importer.hpp>
+#include<assimp/postprocess.h>
 #include<Mesh.hpp>
 #include<Material.hpp>
 #include<map>
@@ -14,29 +15,35 @@ public:
     {
     public:
         Node();
-        void create(const uint32_t maxMeshCount);
+        void create(ObjectManagementStrategy* allocator, const uint32_t maxMeshCount);
         void setMesh(const uint32_t index, Mesh* mesh);
         void addChild(const std::string& name);
         void addChild(const std::string& name, Node&& node);
         Node& operator[](const std::string& key);
         const Node& operator[](const std::string& key) const;
         const Array<Mesh*>& getMeshes() const;
+        const DescriptorInfo& getModelMatrixDescriptor() const;
+        const std::map<std::string, Node>& getChildrenNodes() const;
+        void setModelMatrix(const aiMatrix4x4& mat);
         // rotate, scale, move
         void destroy();
         ~Node();
     private:
         glm::mat4 modelMatrix;
+        BufferInfo modelMatrixBuffer;
+        DescriptorInfo modelMatrixDescriptor;
         Array<Mesh*> meshes;
         std::map<std::string, Node> children;
     };
     Scene();
     void setAllocator(ObjectManagementStrategy* allocator);
     void loadFromFile(const std::string& imagePath, const std::string& file);
-    //void create(const uint32_t maxMeshCount, const uint32_t maxMaterialCount);
     Material& getMaterial(const uint32_t index);
     Mesh& getMesh(const uint32_t index);
     Node& operator[](const std::string& key);
     const Node& operator[](const std::string& key) const;
+    const Node& getRootNode() const;
+    Node& getRootNode();
     void clearExtraResources();
     void destroy();
     ~Scene();
--- RenderSystem/src/Scene.cpp
+++ RenderSystem/src/Scene.cpp
@@ -1,10 +1,35 @@
 #include<Scene.hpp>
+#include<glm/gtx/euler_angles.hpp>
 
 Scene::Node::Node(): modelMatrix(1.0f) {}
 
-void Scene::Node::create(const uint32_t maxMeshCount)
+void Scene::Node::create(ObjectManagementStrategy* allocator, const uint32_t maxMeshCount)
 {
     meshes.create(maxMeshCount);
+    allocator->allocateUniformBuffer(sizeof(modelMatrix), VkShaderStageFlagBits::VK_SHADER_STAGE_VERTEX_BIT | VkShaderStageFlagBits::VK_SHADER_STAGE_GEOMETRY_BIT | VkShaderStageFlagBits::VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT, modelMatrixBuffer, modelMatrixDescriptor);
+    allocator->updateBuffer(&modelMatrix, modelMatrixBuffer);
+}
+#include<iostream>
+void Scene::Node::setModelMatrix(const aiMatrix4x4& mat)
+{
+    aiVector3D aiscale, airotation, aiposition;
+    mat.Decompose(aiscale, airotation, aiposition);
+    glm::vec3 s = {aiscale.x, aiscale.y, aiscale.z}, r = {airotation.x, airotation.y, airotation.z}, p = {aiposition.x, aiposition.y, aiposition.z};
+    std::cout << s.x << ' ' << s.y << ' ' << s.z << " <- scale\n";
+    std::cout << p.x << ' ' << p.y << ' ' << p.z << " <- pos\n";
+    std::cout << r.x << ' ' << r.y << ' ' << r.z << " <- rot\n";
+    modelMatrix *= glm::orientate4(r);
+    modelMatrix = glm::translate(modelMatrix, p);
+    modelMatrix = glm::scale(modelMatrix, s);
+    std::cout << "Model: \n";
+    for(int i = 0; i < 4; ++i)
+    {
+        for(int j = 0; j < 4; ++j)
+        {
+            std::cout << modelMatrix[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
 }
 
 void Scene::Node::setMesh(const uint32_t index, Mesh* mesh)
@@ -37,6 +62,16 @@ const Array<Mesh*>& Scene::Node::getMeshes() const
     return meshes;
 }
 
+const std::map<std::string, Scene::Node>& Scene::Node::getChildrenNodes() const
+{
+    return children;
+}
+
+const DescriptorInfo& Scene::Node::getModelMatrixDescriptor() const
+{
+    return modelMatrixDescriptor;
+}
+
 void Scene::Node::destroy()
 {
     modelMatrix = glm::mat4(1.0f);
@@ -61,13 +96,15 @@ void Scene::setAllocator(ObjectManagementStrategy* allocator)
 
 void Scene::loadNode(const aiNode* ainode, Node& node)
 {
-    node.create(ainode->mNumMeshes);
+    node.create(allocator, ainode->mNumMeshes);
+    node.setModelMatrix(ainode->mTransformation);
     for(auto i = 0; i < ainode->mNumMeshes; ++i)
     {
         node.setMesh(i, &meshes[*(ainode->mMeshes + i)]);
     }
     for(auto i = 0; i < ainode->mNumChildren; ++i)
     {
+        std::cout << (*(ainode->mChildren + i))->mName.C_Str() << " <- child\n";
         node.addChild((*(ainode->mChildren + i))->mName.C_Str());
         loadNode(*(ainode->mChildren + i), node[(*(ainode->mChildren + i))->mName.C_Str()]);
     }
@@ -75,18 +112,27 @@ void Scene::loadNode(const aiNode* ainode, Node& node)
 
 void Scene::loadFromFile(const std::string& imagePath, const std::string& file)
 {
-    importedScene = importer.ReadFile(file, 0);
+    importedScene = importer.ReadFile(file, aiProcess_Triangulate | aiProcess_CalcTangentSpace | aiProcess_JoinIdenticalVertices);
     meshes.create(importedScene->mNumMeshes);
     materials.create(importedScene->mNumMaterials);
     loadMaterials(imagePath);
     loadMeshes();
-    //root.create(scene.mRootNode->mNumMeshes);
     loadNode(importedScene->mRootNode, root);
 }
 
 Material& Scene::getMaterial(const uint32_t index)
 {
     return materials[index];
+}
+
+const Scene::Node& Scene::getRootNode() const
+{
+    return root;
+}
+
+Scene::Node& Scene::getRootNode()
+{
+    return root;
 }
 
 Mesh& Scene::getMesh(const uint32_t index)
