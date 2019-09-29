99a701ea29cd4855ccc41130ebccaf117b482d41
notdatboi <noddatboi@gmail.com>
2019-09-29 21:33:26 +0300

    Fix

--- RenderSystem/include/Constants.hpp
+++ RenderSystem/include/Constants.hpp
@@ -2,17 +2,17 @@
 #define CONSTANTS_HPP
 
 #define MAX_VERTEX_COUNT 100000
-#define MAX_VERTEX_SIZE 24
-#define MAX_IMAGE_DIMENSION 1024
+#define MAX_VERTEX_SIZE 64
+#define MAX_IMAGE_DIMENSION 2048
 #define MAX_TEXTURE_COUNT 10
 #define MAX_UNIFORM_COUNT 15
 
 enum DrawableType
 {
-    NotTextured,
-    Textured,
-    TexturedWithNormalMap,
-    Count
+    DTNotTextured,
+    DTTextured,
+    DTTexturedWithNormalMap,
+    DTCount
 };
 
 #endif
