cd2df780c1597fb490ef9b103e1f0705cf40031c
notdatboi <noddatboi@gmail.com>
2019-09-29 21:39:12 +0300

    Added complete renderer class

--- RenderSystem/include/Renderer.hpp
+++ RenderSystem/include/Renderer.hpp
@@ -3,39 +3,59 @@
 #include<System.hpp>
 #include<Swapchain.hpp>
 #include<RenderPassHolder.hpp>
-#include<CommandPool.hpp>
 #include<SynchronizationPool.hpp>
-#include<ImagePool.hpp>
-
-struct RenderSyncPrimitives
-{
-    uint32_t imageAcquired;
-    uint32_t renderFinished;
-};
+#include<ObjectManagementStrategy.hpp>
+#include<Scene.hpp>
+#include<GraphicsPipelineUtils.hpp>
+#include<Shader.hpp>
 
 class Renderer
 {
 public:
     Renderer();
-    void createBase(const System* system, const CommandPool* commandPool, const SynchronizationPool* syncPool, const ImagePool* imagePool); // command pool must allow individual command buffer reset
-    const uint32_t getSwapchainImageCount() const;
-    void createRenderPass(Array<uint32_t>&& depthAttachmentIndices, const VkFormat depthAttachmentFormat, Array<uint32_t>&& commandBufferIndices, Array<RenderSyncPrimitives>&& semaphoreIndices, Array<RenderSyncPrimitives>&& fenceIndices);
+    void create(const Window& window, const std::vector<std::string>& sceneFilenames, const std::string& imagePath);
+    Scene& getScene(const uint32_t index);
+    const Scene& getScene(const uint32_t index) const;
     void beginRendering();
+    void renderSceneNode(const Scene::Node& node);
     void endRendering();
     void destroy();
     ~Renderer();
 private:
-    const System* system;
-    const CommandPool* commandPool;
-    const SynchronizationPool* syncPool;
-    const ImagePool* imagePool;
+    struct RenderSyncPrimitives
+    {
+        uint32_t imageAcquired;
+        uint32_t renderFinished;
+    };
+    struct ViewProjection
+    {
+        glm::mat4 view;
+        glm::mat4 projection;
+    } viewProj;
+    BufferInfo viewProjBuffer;
+    DescriptorInfo viewProjDescriptor;
+    
+    const uint32_t getSwapchainImageCount() const;
+    void createRenderPass();
+    void createPipelines();
+
+    System system;
     Swapchain swapchain;
     RenderPassHolder renderPass;
-    Array<uint32_t> depthAttachments;
+    Array<Shader> textured; 
+    Array<Shader> notTextured;
+    Array<Shader> normalMapped;
+    PipelinePool pipelinePool;
+    CommandPool commandPool;
+    SynchronizationPool syncPool;
+    ObjectManagementStrategy* allocator;
+    Array<ImageInfo> depthAttachments;
     Array<uint32_t> commandBuffers;
     Array<RenderSyncPrimitives> semaphores;
     Array<RenderSyncPrimitives> fences;
+    Array<Scene> scenes;
     uint32_t currentSubmission = 0;
+    uint32_t usedSubmissions = 0;
     uint32_t currentImage;
 };
 
--- RenderSystem/src/Renderer.cpp
+++ RenderSystem/src/Renderer.cpp
@@ -4,23 +4,69 @@ Renderer::Renderer()
 {
 }
 
-void Renderer::createBase(const System* system, const CommandPool* commandPool, const SynchronizationPool* syncPool, const ImagePool* imagePool)
+void Renderer::create(const Window& window, const std::vector<std::string>& sceneFilenames, const std::string& imagePath)
 {
-    this->system = system;
-    this->commandPool = commandPool;
-    this->syncPool = syncPool;
-    this->imagePool = imagePool;
+    VkPhysicalDeviceFeatures features = {};
+    features.logicOp = VK_TRUE;
+    system.create(window, true, features);
+    uint32_t swapchainImgCount;
+    swapchain.create(&system, swapchainImgCount);
+    commandPool.create(&system, true);
+    syncPool.create(&system);
+    allocator = new SharedMemoryObjectManagementStrategy();
+    allocator->create(&system, &syncPool, &commandPool);
+
+    viewProj.view = glm::lookAt(glm::vec3(8, 5, 7), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    viewProj.projection = glm::perspective(glm::radians(60.0f), (float)swapchain.getExtent().width / swapchain.getExtent().height, 0.1f, 100.0f);
+    allocator->allocateUniformBuffer(sizeof(viewProj), VkShaderStageFlagBits::VK_SHADER_STAGE_VERTEX_BIT | VkShaderStageFlagBits::VK_SHADER_STAGE_GEOMETRY_BIT | VkShaderStageFlagBits::VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT, viewProjBuffer, viewProjDescriptor);
+    allocator->updateBuffer(&viewProj, viewProjBuffer);
+
+    scenes.create(sceneFilenames.size());
+    for(auto ind = 0; ind < sceneFilenames.size(); ++ind)
+    {
+        scenes[ind].setAllocator(allocator);
+        scenes[ind].loadFromFile(imagePath, sceneFilenames[ind]);
+    }
+
+    depthAttachments.create(swapchainImgCount);
+    for(auto ind = 0; ind < swapchainImgCount; ++ind)
+    {
+        allocator->allocateDepthMap(swapchain.getExtent(), depthAttachments[ind]);
+    }
+
+    allocator->load();
+    allocator->update();
+
+    for(auto ind = 0; ind < sceneFilenames.size(); ++ind)
+    {
+        scenes[ind].clearExtraResources();
+    }
+
+    createRenderPass();
+    createPipelines();
+}
+
+Scene& Renderer::getScene(const uint32_t index)
+{
+    return scenes[index];
+}
+
+const Scene& Renderer::getScene(const uint32_t index) const
+{
+    return scenes[index];
 }
 
 const uint32_t Renderer::getSwapchainImageCount() const
 {
-    swapchain.getImageCount();
+    return swapchain.getImageCount();
 }
 
-void Renderer::createRenderPass(Array<uint32_t>&& depthAttachmentIndices, const VkFormat depthAttachmentFormat, Array<uint32_t>&& commandBufferIndices, Array<RenderSyncPrimitives>&& semaphoreIndices, Array<RenderSyncPrimitives>&& fenceIndices)
+void Renderer::createRenderPass()
 {
-    uint32_t swapchainImgCount;
-    swapchain.create(system, swapchainImgCount);
+    uint32_t swapchainImgCount = getSwapchainImageCount();
+    VkFormat depthAttachmentFormat;
+    VkImageTiling depthAttachmentTiling;
+    allocator->pickDepthStencilFormat(depthAttachmentFormat, depthAttachmentTiling);
     Array<VkAttachmentDescription> attachments(2);
     attachments[0] = 
     {
@@ -71,25 +117,46 @@ void Renderer::createRenderPass(Array<uint32_t>&& depthAttachmentIndices, const 
         0,
         nullptr
     };
-    Array<VkSubpassDependency> dependencies;
-    renderPass.create(system, attachments, subpasses, dependencies, swapchainImgCount);
-    depthAttachments = depthAttachmentIndices;
-    commandBuffers = commandBufferIndices;
-    semaphores = semaphoreIndices;
-    fences = fenceIndices;
+    Array<VkSubpassDependency> dependencies(1);
+    dependencies[0] = 
+    {
+        VK_SUBPASS_EXTERNAL,
+        0,
+        VkPipelineStageFlagBits::VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+        VkPipelineStageFlagBits::VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+        VkAccessFlags(),
+        VkAccessFlagBits::VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VkAccessFlagBits::VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT
+    };
+    renderPass.create(&system, attachments, subpasses, dependencies, swapchainImgCount);
+
+    commandBuffers.create(swapchainImgCount);
+    semaphores.create(swapchainImgCount);
+    fences.create(swapchainImgCount);
+    uint32_t firstSemaphore = syncPool.getSemaphoreCount(), firstFence = syncPool.getFenceCount();
+    syncPool.addSemaphores(swapchainImgCount * 2);
+    syncPool.addFences(swapchainImgCount, false);
+    syncPool.addFences(swapchainImgCount, false);
+    uint32_t firstCommandBuffer = commandPool.getCurrentPoolSize();
+    commandPool.addCommandBuffers(swapchainImgCount);
+    commandPool.allocateCommandBuffers(firstCommandBuffer, swapchainImgCount, VkCommandBufferLevel::VK_COMMAND_BUFFER_LEVEL_PRIMARY);
     for(auto ind = 0; ind < swapchainImgCount; ++ind)
     {
-        Array<VkImageView> attachments = {swapchain.getView(ind), (*imagePool)[depthAttachments[ind]].view};
+        semaphores[ind].imageAcquired = firstSemaphore + ind * 2;
+        semaphores[ind].renderFinished = firstSemaphore + ind * 2 + 1;
+        fences[ind].imageAcquired = firstFence + ind;
+        fences[ind].renderFinished = firstFence + ind + swapchainImgCount;
+        commandBuffers[ind] = firstCommandBuffer + ind;
+        Array<VkImageView> attachments = {swapchain.getView(ind), depthAttachments[ind].holder->getView(depthAttachments[ind].viewIndex)};
         renderPass.createFramebuffer(ind, attachments, swapchain.getExtent());
     }
 }
-
+#include<iostream>
 void Renderer::beginRendering()
 {
-    syncPool->waitForFences(fences[currentSubmission].renderFinished);
-    syncPool->resetFences(fences[currentSubmission].renderFinished);
-    commandPool->reset(commandBuffers[currentImage]);
-    currentImage = swapchain.acquireNextImage(syncPool->getSemaphore(semaphores[currentSubmission].imageAcquired), syncPool->getFence(fences[currentSubmission].imageAcquired));
+    allocator->update();
+    commandPool.reset(commandBuffers[currentSubmission], true);
+    currentImage = swapchain.acquireNextImage(syncPool.getSemaphore(semaphores[currentSubmission].imageAcquired), syncPool.getFence(fences[currentSubmission].imageAcquired));
+    std::cout << "Current sub: " << currentSubmission << " Current image: " << currentImage << " Used subs: " << usedSubmissions << '\n';
 
     VkCommandBufferBeginInfo beginInfo = 
     {
@@ -99,13 +166,23 @@ void Renderer::beginRendering()
         nullptr
     };
 
-    vkBeginCommandBuffer((*commandPool)[commandBuffers[currentSubmission]], &beginInfo);
+    VkImageSubresourceRange subresource = 
+    {
+        VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+        0,
+        1,
+        0,
+        1
+    };
+
+    vkBeginCommandBuffer(commandPool[commandBuffers[currentSubmission]], &beginInfo);
+    ImageHolder::recordLayoutChangeCommands(commandPool[commandBuffers[currentSubmission]], (!(usedSubmissions & (1 << currentSubmission))) ? VkImageLayout::VK_IMAGE_LAYOUT_UNDEFINED : VkImageLayout::VK_IMAGE_LAYOUT_PRESENT_SRC_KHR, VkImageLayout::VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, swapchain.getImage(currentImage), subresource);
 
     VkRect2D renderArea;
     renderArea.extent = swapchain.getExtent();
     renderArea.offset = {0, 0};
     VkClearValue clearVals[2];
-    clearVals[0].color = {0, 0, 0, 1.0};
+    clearVals[0].color = {0, 0, 0, 0};//{1 * (currentSubmission == 0), 1 * (currentSubmission == 1), 1 * (currentSubmission == 2), 1.0};
     clearVals[1].depthStencil = {1, 0};
     
     VkRenderPassBeginInfo renderPassInfo = 
@@ -118,12 +195,79 @@ void Renderer::beginRendering()
         2,
         clearVals
     };
-    vkCmdBeginRenderPass((*commandPool)[commandBuffers[currentSubmission]], &renderPassInfo, VkSubpassContents::VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBeginRenderPass(commandPool[commandBuffers[currentSubmission]], &renderPassInfo, VkSubpassContents::VK_SUBPASS_CONTENTS_INLINE);
+}
+
+void Renderer::renderSceneNode(const Scene::Node& node)
+{
+    static DrawableType prevType = DrawableType::DTCount;
+    const VkCommandBuffer& commands = commandPool[commandBuffers[currentSubmission]];
+    const uint32_t meshCount = node.getMeshes().getSize();
+    for(auto meshInd = 0; meshInd < meshCount; ++meshInd)
+    {
+        const Mesh* mesh = node.getMeshes()[meshInd];
+        const DrawableType type = mesh->getMaterial()->getType();
+        const DescriptorInfo& nodeModelDescriptor = node.getModelMatrixDescriptor();
+        if(prevType != type)
+        {
+            vkCmdBindPipeline(commands, 
+                VkPipelineBindPoint::VK_PIPELINE_BIND_POINT_GRAPHICS, 
+                pipelinePool[type]);
+
+            const auto& matDescriptors = mesh->getMaterial()->getDescriptorInfos();
+            Array<VkDescriptorSet> sets(2 + matDescriptors.getSize());
+            sets[0] = (*viewProjDescriptor.pool)[viewProjDescriptor.setIndex];
+            sets[1] = (*nodeModelDescriptor.pool)[nodeModelDescriptor.setIndex];
+            for(auto ind = 2; ind < sets.getSize(); ++ind)
+            {
+                sets[ind] = (*matDescriptors[ind - 2].pool)[matDescriptors[ind - 2].setIndex];
+            }
+
+            vkCmdBindDescriptorSets(commands, 
+                VkPipelineBindPoint::VK_PIPELINE_BIND_POINT_GRAPHICS, 
+                allocator->getPipelineLayout(type),
+                0,
+                sets.getSize(),
+                sets.getPtr(),
+                0,
+                nullptr);
+        }
+        else
+        {
+            const auto& matDescriptors = mesh->getMaterial()->getDescriptorInfos();
+            Array<VkDescriptorSet> sets(1 + matDescriptors.getSize());
+            sets[0] = (*nodeModelDescriptor.pool)[nodeModelDescriptor.setIndex];
+            for(auto ind = 1; ind < sets.getSize(); ++ind)
+            {
+                sets[ind] = (*matDescriptors[ind - 1].pool)[matDescriptors[ind - 1].setIndex];
+            }
+            vkCmdBindDescriptorSets(commands, 
+                VkPipelineBindPoint::VK_PIPELINE_BIND_POINT_GRAPHICS, 
+                allocator->getPipelineLayout(type),
+                1,
+                sets.getSize(),
+                sets.getPtr(),
+                0,
+                nullptr);
+        }
+        const BufferInfo& vb = mesh->getVertexBuffer(), ib = mesh->getIndexBuffer();
+        vkCmdBindVertexBuffers(commands, 0, 1, &(*vb.holder)[vb.index], &vb.offset);
+        vkCmdBindIndexBuffer(commands, (*ib.holder)[ib.index], ib.offset, VkIndexType::VK_INDEX_TYPE_UINT32);
+        vkCmdDrawIndexed(commands, mesh->getIndexCount(), 1, 0, 0, 0);
+        prevType = type;
+    }
+
+    for(const auto& kvPair : node.getChildrenNodes())
+    {
+        renderSceneNode(kvPair.second);
+    }
+
+    prevType = DrawableType::DTCount;
 }
 
 void Renderer::endRendering()
 {
-    const VkCommandBuffer& commands = (*commandPool)[commandBuffers[currentSubmission]];
+    const VkCommandBuffer& commands = commandPool[commandBuffers[currentSubmission]];
     vkCmdEndRenderPass(commands);
     vkEndCommandBuffer(commands);
 
@@ -134,17 +278,17 @@ void Renderer::endRendering()
         VK_STRUCTURE_TYPE_SUBMIT_INFO,
         nullptr,
         1,
-        &syncPool->getSemaphore(semaphores[currentSubmission].imageAcquired),
+        &syncPool.getSemaphore(semaphores[currentSubmission].imageAcquired),
         &dstFlags,
         1,
         &commands,
         1,
-        &syncPool->getSemaphore(semaphores[currentSubmission].renderFinished)
+        &syncPool.getSemaphore(semaphores[currentSubmission].renderFinished)
     };
 
-    syncPool->waitForFences(fences[currentSubmission].imageAcquired);
-    syncPool->resetFences(fences[currentSubmission].imageAcquired);
-    checkResult(vkQueueSubmit(system->getPresentQueue().queue, 1, &submit, syncPool->getFence(fences[currentSubmission].renderFinished)), "Queue submission failed.\n");
+    syncPool.waitForFences(fences[currentSubmission].imageAcquired);
+    syncPool.resetFences(fences[currentSubmission].imageAcquired);
+    checkResult(vkQueueSubmit(system.getGraphicsQueue().queue, 1, &submit, syncPool.getFence(fences[currentSubmission].renderFinished)), "Queue submission failed.\n");
 
     VkResult presentResult;
     VkPresentInfoKHR present = 
@@ -152,27 +296,129 @@ void Renderer::endRendering()
         VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
         nullptr,
         1,
-        &syncPool->getSemaphore(semaphores[currentSubmission].renderFinished),
+        &syncPool.getSemaphore(semaphores[currentSubmission].renderFinished),
         1,
         &swapchain.getSwapchain(),
         &currentImage,
         &presentResult
     };
 
-    checkResult(vkQueuePresentKHR(system->getPresentQueue().queue, &present), "Present submission failed.\n");
+    syncPool.waitForFences(fences[currentSubmission].renderFinished);
+    syncPool.resetFences(fences[currentSubmission].renderFinished);
+
+    checkResult(vkQueuePresentKHR(system.getPresentQueue().queue, &present), "Present submission failed.\n");
     checkResult(presentResult, "Image presentation failed.\n");
 
+    if(!(usedSubmissions & (1 << currentSubmission))) usedSubmissions |= (1 << currentSubmission);
     currentSubmission = (currentSubmission + 1) % swapchain.getImageCount();
+}
+
+void Renderer::createPipelines()
+{
+    textured.create(2);
+    notTextured.create(2);
+    normalMapped.create(2);
+    textured[0].create(&system, "RenderSystem/shaders/Textured.vert");
+    textured[1].create(&system, "RenderSystem/shaders/Textured.frag");
+    notTextured[0].create(&system, "RenderSystem/shaders/NotTextured.vert");
+    notTextured[1].create(&system, "RenderSystem/shaders/NotTextured.frag");
+    normalMapped[0].create(&system, "RenderSystem/shaders/NormalMapped.vert");
+    normalMapped[1].create(&system, "RenderSystem/shaders/NormalMapped.frag");
+    Array<ShaderStageInfo> texturedInfos(textured.getSize()), notTexturedInfos(notTextured.getSize()), normalMappedInfos(normalMapped.getSize());
+    for(auto ind = 0; ind < textured.getSize(); ++ind) texturedInfos[ind] = textured[ind].getShader();
+    for(auto ind = 0; ind < notTextured.getSize(); ++ind) notTexturedInfos[ind] = notTextured[ind].getShader();
+    for(auto ind = 0; ind < normalMapped.getSize(); ++ind) normalMappedInfos[ind] = normalMapped[ind].getShader();
+
+    Array<VkViewport> viewports(1);
+    viewports[0] = 
+    {
+        0,
+        0,
+        static_cast<float>(swapchain.getExtent().width),
+        static_cast<float>(swapchain.getExtent().height),
+        0,
+        1
+    };
+    Array<VkRect2D> scissors(1);
+    scissors[0] = 
+    {
+        {0, 0},
+        {static_cast<uint32_t>(viewports[0].width), static_cast<uint32_t>(viewports[0].height)}
+    };
+    Array<VkVertexInputBindingDescription> bindings;
+    Array<VkVertexInputAttributeDescription> attributes;
+    VertexBufferStandard vbs;
+    VertexBufferNotTextured vbnt;
+    VertexBufferWithNormalMap vbnm;
+    Array<VkPipelineColorBlendAttachmentState> colorBlendStates(1);
+    colorBlendStates[0] = 
+    {
+        VK_FALSE,
+        VkBlendFactor::VK_BLEND_FACTOR_SRC_COLOR,
+        VkBlendFactor::VK_BLEND_FACTOR_DST_COLOR,
+        VkBlendOp::VK_BLEND_OP_ADD,
+        VkBlendFactor::VK_BLEND_FACTOR_SRC_ALPHA,
+        VkBlendFactor::VK_BLEND_FACTOR_DST_ALPHA,
+        VkBlendOp::VK_BLEND_OP_ADD,
+        VkColorComponentFlagBits::VK_COLOR_COMPONENT_R_BIT | VkColorComponentFlagBits::VK_COLOR_COMPONENT_G_BIT | VkColorComponentFlagBits::VK_COLOR_COMPONENT_B_BIT | VkColorComponentFlagBits::VK_COLOR_COMPONENT_A_BIT
+    };
+    pipelinePool.create(&system, DrawableType::DTCount);
+    PipelineInfoBuilder infoBuilder;
+    infoBuilder.setShaderStages(texturedInfos);
+    vbs.getGraphicsPipelineVertexInputState(bindings, attributes);
+    infoBuilder.setVertexInputState(true, bindings, attributes);
+    infoBuilder.setInputAssemblyState(true);
+    infoBuilder.setTessellationState(false);
+    infoBuilder.setViewportState(true, viewports, scissors);
+    infoBuilder.setRasterizationState(VK_FALSE, VkPolygonMode::VK_POLYGON_MODE_FILL, VkCullModeFlagBits::VK_CULL_MODE_BACK_BIT, VkFrontFace::VK_FRONT_FACE_COUNTER_CLOCKWISE);
+    infoBuilder.setMultisampleState();
+    infoBuilder.setDepthStencilState();
+    infoBuilder.setColorBlendState(true, VK_FALSE, VkLogicOp(), colorBlendStates);
+    infoBuilder.setDynamicState(false);
+    infoBuilder.setLayout(&allocator->getPipelineLayout(DrawableType::DTTextured));
+    infoBuilder.setRenderPass(&renderPass.getRenderPass(), 0);
+
+    pipelinePool.createPipeline(DrawableType::DTTextured, infoBuilder.generatePipelineInfo());
+
+    infoBuilder.setShaderStages(notTexturedInfos);
+    vbnt.getGraphicsPipelineVertexInputState(bindings, attributes);
+    infoBuilder.setVertexInputState(true, bindings, attributes);
+    infoBuilder.setLayout(&allocator->getPipelineLayout(DrawableType::DTNotTextured));
+
+    pipelinePool.createPipeline(DrawableType::DTNotTextured, infoBuilder.generatePipelineInfo());
+
+    infoBuilder.setShaderStages(normalMappedInfos);
+    vbnm.getGraphicsPipelineVertexInputState(bindings, attributes);
+    infoBuilder.setVertexInputState(true, bindings, attributes);
+    infoBuilder.setLayout(&allocator->getPipelineLayout(DrawableType::DTTexturedWithNormalMap));
+
+    pipelinePool.createPipeline(DrawableType::DTTexturedWithNormalMap, infoBuilder.generatePipelineInfo());
 }
 
 void Renderer::destroy()
 {
-    renderPass.destroy();
+    if(system.getDevice()) vkDeviceWaitIdle(system.getDevice());
+    else return;
     swapchain.destroy();
-    depthAttachments.clean();
-    commandBuffers.clean();
-    semaphores.clean();
-    fences.clean();
+    renderPass.destroy();
+    pipelinePool.destroy();
+    //for(auto ind = 0; ind < textured.getSize(); ++ind) textured[ind].destroy();
+    textured.clear();
+    //for(auto ind = 0; ind < notTextured.getSize(); ++ind) notTextured[ind].destroy();
+    notTextured.clear();
+    //for(auto ind = 0; ind < normalMapped.getSize(); ++ind) normalMapped[ind].destroy();
+    normalMapped.clear();
+    commandPool.destroy();
+    syncPool.destroy();
+    depthAttachments.clear();
+    commandBuffers.clear();
+    semaphores.clear();
+    fences.clear();
+    scenes.clear();
+    allocator->destroy();
+    delete allocator;
+
+    system.destroy();
 }
 
 Renderer::~Renderer()
